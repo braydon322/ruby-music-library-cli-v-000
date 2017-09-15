610a735f44b21837ff3d5a0facfe568f9109a5f5
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-15 23:11:16 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -105,6 +105,7 @@ class MusicLibraryController
 
     song = Song.all[song_choice]
 
+    
     if song != nil && (song_choice < Song.all.length && song_choice > 1)
       puts "Playing #{song.name} by #{song.artist.name}"
     end
