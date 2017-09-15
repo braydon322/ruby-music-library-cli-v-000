725e344385ca0c60bbdea2a07fee0ea34464f7d1
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-15 23:20:45 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -103,11 +103,12 @@ class MusicLibraryController
 
     Song.all.sort_by(&:name)
 
+
     song = Song.all[song_choice]
     binding.pry
     puts "#{song.name} - #{song.artist.name}"
 
-    if song != nil && (song_choice < Song.all.length && song_choice > 1)
+    if song && (song_choice < Song.all.length && song_choice > 1)
       puts "Playing #{song.name} by #{song.artist.name}"
     end
 
